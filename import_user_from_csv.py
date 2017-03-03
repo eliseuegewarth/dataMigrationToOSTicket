c35688034f961a29249582a2d07a2591ff783e81
Eliseu Egewarth <eliseuegewarth@gmail.com>
2017-03-03 13:18:22 -0300

    Update import_user_from_csv.py

--- import_user_from_csv.py
+++ import_user_from_csv.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import csv
 import sys
-
+from datetime import date, datetime
 import mysql.connector
 
 def main(argv):
@@ -9,31 +9,34 @@ def main(argv):
 
 def import_organization_from_csv(filename):
     ## Do not run the command below in production ##
-    truncate()
+    # truncate()
 
     cnx = mysql.connector.connect(user='osticket', password='secret', database='osticket', host='127.0.0.1', port='3306')
     cursor = cnx.cursor()
 
-    add_organization = ("INSERT INTO ost_user "
-                        "(name, created) "
-                        "VALUES (%(name)s, %(created)s)"
+    add_user = ("INSERT INTO ost_user "
+                        "(name, created, updated) "
+                        "VALUES (%(name)s, %(created)s, %(updated)s)"
                         )
     id_position = 0
-    org_name_position = 1
+    user_name_position = 1
 
-
+    now = datetime.now().date()
+    
+    #  ## change to open csv id map ## with open(filename) as csvfile:
     with open(filename) as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         first = True
         for row in spamreader:
             if not first:
-                data_organization = {
-                    'name': row[org_name_position],
-                    'created': CURRENT_TIMESTAMP,
-                    }
-                cursor.execute(add_organization, data_organization)
-                last_organization = cursor.lastrowid
-                csv_template_string = '"{0}","{1}"'.format(row[id_position] , last_organization)
+                data_user = {
+                    'name': row[user_name_position],
+                    'created': now,
+                    'updated': now,
+                }
+                cursor.execute(add_user, data_user)
+                last_user = cursor.lastrowid
+                csv_template_string = '"{0}","{1}"'.format(row[id_position] , last_user)
                 print (csv_template_string)
                 
             else:
